2824092b79435b68b932c91b28778e39b9790521
Handing Zhang <zhanghanding1997@gmail.com>
2023-11-12 15:36:55 -0500

    added across vs modify_if

---
+++ handy_tricks/modify_columns.R
@@ -0,0 +1,34 @@
+# We want to turn all character columns into factor columns
+
+# dplyr::mutate(across(where)) 
+#      vs 
+# purrr::modify_if()
+
+library(dplyr)
+library(purrr)
+
+
+# mutate ####
+# across() makes it easy to apply the same transformation to multiple columns,
+# allowing you to use select() semantics inside in "data-masking" functions like summarise() and mutate().
+# See vignette("colwise") for more details.
+
+(iris_1 <- iris %>% 
+  mutate(across(.cols = where(is.character),  # Columns to transform. 
+                .fns = as.factor)) %>% # Functions to apply to each of the selected columns. 
+                                       # A function, e.g. mean.
+                                       # A purrr-style lambda, e.g. ~ mean(.x, na.rm = TRUE)
+    as_tibble())
+  
+
+# modify_if ####
+# the modify() family always returns the same type as the input object.
+# in this case a dataframe
+# modify_if() only modifies the elements of x that satisfy a predicate and leaves the others unchanged.
+(iris_1 <- iris %>%
+    modify_if(.p = is.character, 
+              .f = as.factor))
+
+# A single predicate function, a formula describing such a predicate function, or a logical vector of the same length as .x. 
+# Only those elements where .p evaluates to TRUE will be modified.
+
